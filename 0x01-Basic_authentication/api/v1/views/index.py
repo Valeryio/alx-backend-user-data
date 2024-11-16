9669c37456ba3eddd04b95e9677a6dcb02bc18db
Valeryio <tommagoma23@gmail.com>
2024-11-16 09:01:43 +0000

    Update the task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,17 +13,20 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+
 @app_views.route("/unauthorized/", methods=['GET'], strict_slashes=False)
 def unauthorized():
     """This view is unauthorized
     """
     abort(401)
 
+
 @app_views.route("/forbidden/", methods=['GET'], strict_slashes=False)
 def forbidden():
     """This view is forbidden
     """
     abort(403)
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -35,4 +38,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
