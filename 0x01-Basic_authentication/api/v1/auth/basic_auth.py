dd5e5b5aa4a4b37bdaaa2d49610aa5ea1a5ec64a
Valeryio <tommagoma23@gmail.com>
2024-11-16 09:10:53 +0000

    Update the task 6

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
+"""This is the basic auth module
+"""
+
 import base64
 import binascii
 import models.user import User
 from api.v1.auth.auth import Auth
-
-"""This is the basic auth module
-"""
 
 
 class BasicAuth(Auth):
