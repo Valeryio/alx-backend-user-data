168e0c9e64e60213352c2dde3f300ea97bbe1b1a
Valeryio <tommagoma23@gmail.com>
2024-11-19 09:35:23 +0000

    Commit the task 1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,8 +4,8 @@
 """
 
 from typing import Optional
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
@@ -17,8 +17,8 @@ class User(Base):
     """
     __tablename__: str = "users"
 
-    id: int = Column(Integer, primary_key=True, index=True)
-    email: str = Column(String(250), nullable=False)
-    hashed_password: str = Column(String(250), nullable=False, index=True)
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250))
+    hashed_password: str = Column(String(250), nullable=False)
     session_id: Optional[str] = Column(String(250))
     reset_token: Optional[str] = Column(String(250))
